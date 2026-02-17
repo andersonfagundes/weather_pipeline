a0da61f387a6de88673fd02ad4e50a627c596534
Anderson Fagundes <anderson.fagundes4@gmail.com>
2026-02-17 18:10:03 +0000

    feat: add API data extraction script

---
+++ src/extract_data.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from pathlib import Path
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# api_key = '1f909455753ac623ef20dc4038097067'
+# url = f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&units=metric&appid={api_key}'
+
+def extract_weather_data(url:str)-> list:
+  response = requests.get(url)
+  data = response.json()
+
+  if response.status_code != 200:
+    logging.error("Error in the request")
+    return []
+  
+  if not data:
+      logging.warn("No data returned")
+      return []
+
+  output_path = 'data/weather_data.json'
+  output_dir = Path(output_path).parent
+  output_dir.mkdir(parents=True, exist_ok=True)
+
+  with open(output_path, 'w') as f:
+    json.dump(data, f, indent=4)
+
+  logging.info("File saved in {output_path}")
+  return data
+
+#extract_weather_data(url)
+# if you want to test, run this command
+# uv run src/extract_data.py
